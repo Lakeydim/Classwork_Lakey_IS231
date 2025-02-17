756828eaf81c393a95fdbf33edaf86893c6e5f70
Lakeydim <88690846+Lakeydim@users.noreply.github.com>
2025-02-17 12:52:23 +0900

    Delete 3.1.kt

--- 3.1.kt
+++
@@ -1,8 +0,0 @@
-fun main() {
-    println("Введите три числа:")
-    val num1 = readLine()!!.toInt()
-    val num2 = readLine()!!.toInt()
-    val num3 = readLine()!!.toInt()
-    val maxNum = maxOf(num1, num2, num3)
-    println("Максимальное число: $maxNum")
-}
