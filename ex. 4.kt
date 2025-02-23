22649915ff491eaf7cf3c652df4770e486128783
Lakeydim <88690846+Lakeydim@users.noreply.github.com>
2025-02-24 08:17:51 +0900

    Delete ex. 4.kt

--- ex. 4.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    println("Введите число:")
-    val input = readLine()
-        if (input != null) {
-        println("Вы ввели число: $input")
-    } else {
-        println("Ошибка: не было введено число.")
-    }
-}
